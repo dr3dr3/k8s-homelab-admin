b7b78b16c6da06103ecc82855d55a5ab8a0c3e7d
Andre Dreyer <git@andredreyer.com>
2025-07-14 10:41:26 +0000

    Install script for dotfiles

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-# Install dotfiles
-echo "Installing dotfiles..."
+# Devpod runs this script when using dotfiles
 
+# Install dotfiles
+mv ~/dotfiles/.dotfiles ~
+cd ~/.dotfiles
+stow fish starship
+
+# Set git user and email from dotfiles
+cd ~/dotfiles
+./setup.sh
+
+exit 0
